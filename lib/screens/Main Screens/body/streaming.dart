89387a2ebc6f9a4a718d6867dba02d0bb3929458
Boanerges1996 <samsonnkrumah253@gmail.com>
2022-09-04 02:22:45 +0530

    Bottom sheet

--- lib/screens/Main Screens/body/streaming.dart
+++ lib/screens/Main Screens/body/streaming.dart
@@ -7,6 +7,7 @@ import 'package:pp_stream_mobile_app/constant/colors.dart';
 import 'package:pp_stream_mobile_app/constant/page_routes.dart';
 import 'package:pp_stream_mobile_app/providers/user.dart';
 import 'package:pp_stream_mobile_app/screens/Main%20Screens/body/streaming_card_tile.dart';
+import 'package:pp_stream_mobile_app/widgets/reusable.dart';
 import 'package:provider/provider.dart';
 
 class StreamingPage extends StatefulWidget {
@@ -52,7 +53,8 @@ class _StreamingPageState extends State<StreamingPage> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      Navigator.of(context).popAndPushNamed(liveStreamPageRoute);
+                      // Navigator.of(context).popAndPushNamed(liveStreamPageRoute);
+                      _modalBottomSheetMenu();
                     },
                     child: RichText(
                       text: const TextSpan(
@@ -115,7 +117,7 @@ class _StreamingPageState extends State<StreamingPage> {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
+                                  const Text(
                                     "Pied Piper Live",
                                     overflow: TextOverflow.ellipsis,
                                     style: TextStyle(
@@ -168,7 +170,7 @@ class _StreamingPageState extends State<StreamingPage> {
                       padding: const EdgeInsets.only(top: 20.0),
                       child: Row(
                         children: [
-                          Text(
+                          const Text(
                             'Popular Streams',
                             style: TextStyle(
                               color: Colors.white70,
@@ -193,15 +195,15 @@ class _StreamingPageState extends State<StreamingPage> {
                                 child: Row(
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    CircleAvatar(
+                                    const CircleAvatar(
                                       radius: 24, // Image radius
                                       backgroundImage: NetworkImage(
                                           'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/300px-Gull_portrait_ca_usa.jpg'),
                                     ),
-                                    SizedBox(
+                                    const SizedBox(
                                       width: 10,
                                     ),
-                                    Text(
+                                    const Text(
                                       "Anshul Sharma",
                                       style: TextStyle(
                                           fontSize: 18,
@@ -222,8 +224,8 @@ class _StreamingPageState extends State<StreamingPage> {
                             ),
                             child: new DecoratedBox(
                               decoration: new BoxDecoration(
-                                  color: Color.fromRGBO(36, 52, 71, 0.5),
-                                  borderRadius: BorderRadius.all(Radius.circular(10))),
+                                  color: const Color.fromRGBO(36, 52, 71, 0.5),
+                                  borderRadius: const BorderRadius.all(Radius.circular(10))),
                             ),
                           ),
                         ],
@@ -236,6 +238,64 @@ class _StreamingPageState extends State<StreamingPage> {
           ),
         ),
       ),
+    );
+  }
+
+  void _modalBottomSheetMenu() {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(50),
+        ),
+      ),
+      builder: (builder) {
+        return Container(
+          height: 350.0,
+          color: Colors.transparent, //could change this to Color(0xFF737373),
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(
+                  30.0,
+                ),
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                RoundedInput(
+                  onSaved: (data) {},
+                  onChanged: (data) {},
+                  hintText: "Stream Title",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                RoundedInput(
+                  onSaved: (data) {},
+                  onChanged: (data) {},
+                  hintText: "Bio",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                roundedButton(
+                  onTap: () {},
+                  text: "Go Live",
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  backgroundColor: primaryColor,
+                  textColor: Colors.white,
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
