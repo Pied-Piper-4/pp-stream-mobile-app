40ba790ff13fb7b7d04df2554d782cba8a933a6f
Boanerges1996 <samsonnkrumah253@gmail.com>
2022-09-04 09:53:39 +0530

    changes

--- lib/screens/Main Screens/body/streaming.dart
+++ lib/screens/Main Screens/body/streaming.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:pp_stream_mobile_app/constant/colors.dart';
 import 'package:pp_stream_mobile_app/constant/page_routes.dart';
-<<<<<<< HEAD
 import 'package:pp_stream_mobile_app/models/meeting.model.dart';
 import 'package:pp_stream_mobile_app/providers/meeting.provider.dart';
 import 'package:pp_stream_mobile_app/providers/user.dart';
@@ -10,9 +9,6 @@ import 'package:pp_stream_mobile_app/screens/Main%20Screens/body/streaming_card_
 import 'package:pp_stream_mobile_app/services/meeting.request.dart';
 import 'package:pp_stream_mobile_app/utils/interfaces/api_response.dart';
 import 'package:pp_stream_mobile_app/widgets/reusable.dart';
-=======
-import 'package:pp_stream_mobile_app/providers/user.dart';
->>>>>>> 6c84c92cac2394806744724a1566ace46854cad6
 import 'package:provider/provider.dart';
 
 class StreamingPage extends StatefulWidget {
@@ -31,10 +27,8 @@ class _StreamingPageState extends State<StreamingPage> {
       backgroundColor: primaryColor,
       body: Container(
         child: Padding(
-          padding: EdgeInsets.only(
-              left: 14.0,
-              right: 14,
-              top: MediaQuery.of(context).size.height / 15),
+          padding:
+              EdgeInsets.only(left: 14.0, right: 14, top: MediaQuery.of(context).size.height / 15),
           child: Column(
             children: [
               Row(
@@ -62,8 +56,7 @@ class _StreamingPageState extends State<StreamingPage> {
                   ),
                   GestureDetector(
                     onTap: () {
-                      Navigator.of(context)
-                          .popAndPushNamed(liveStreamPageRoute);
+                      Navigator.of(context).popAndPushNamed(liveStreamPageRoute);
                     },
                     child: RichText(
                       text: const TextSpan(
@@ -107,8 +100,7 @@ class _StreamingPageState extends State<StreamingPage> {
                       child: const DecoratedBox(
                         decoration: BoxDecoration(
                             color: Color.fromRGBO(36, 52, 71, 0.5),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
+                            borderRadius: BorderRadius.all(Radius.circular(10))),
                       ),
                     ),
                     Padding(
@@ -118,8 +110,7 @@ class _StreamingPageState extends State<StreamingPage> {
                         children: [
                           CircleAvatar(
                             radius: 26, // Image radius
-                            backgroundImage: NetworkImage(userDataProv
-                                    .user?.pic ??
+                            backgroundImage: NetworkImage(userDataProv.user?.pic ??
                                 "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/300px-Gull_portrait_ca_usa.jpg"),
                           ),
                           const SizedBox(
@@ -133,8 +124,7 @@ class _StreamingPageState extends State<StreamingPage> {
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width / 1.5,
+                                    width: MediaQuery.of(context).size.width / 1.5,
                                     child: const Text(
                                       "Pied Piper Live",
                                       overflow: TextOverflow.ellipsis,
@@ -203,62 +193,7 @@ class _StreamingPageState extends State<StreamingPage> {
                         ],
                       ),
                     ),
-<<<<<<< HEAD
                     meetingsList(context),
-=======
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 18.0,
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned.fill(
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 14.0, bottom: 10.0),
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    CircleAvatar(
-                                      radius: 24, // Image radius
-                                      backgroundImage: NetworkImage(
-                                          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/300px-Gull_portrait_ca_usa.jpg'),
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Text(
-                                      userDataProv.user!.name!,
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          ConstrainedBox(
-                            constraints: new BoxConstraints(
-                              minHeight: 190.0,
-                              minWidth: 240.0,
-                              maxHeight: 190.0,
-                              maxWidth: 240.0,
-                            ),
-                            child: new DecoratedBox(
-                              decoration: new BoxDecoration(
-                                  color: Color.fromRGBO(36, 52, 71, 0.5),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10))),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
->>>>>>> 6c84c92cac2394806744724a1566ace46854cad6
                   ],
                 ),
               ),
@@ -270,7 +205,6 @@ class _StreamingPageState extends State<StreamingPage> {
   }
 
   Future<void> goLive(context) async {}
-<<<<<<< HEAD
 
   Widget meetingsList(context) {
     final meetingProv = Provider.of<MeetingsProvider>(context);
@@ -310,6 +244,4 @@ class _StreamingPageState extends State<StreamingPage> {
       ),
     );
   }
-=======
->>>>>>> 6c84c92cac2394806744724a1566ace46854cad6
 }
